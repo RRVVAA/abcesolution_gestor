427a6fee11acc0d24dd3f15bf9ebbe46670e85de
Jeferson Maciel <ti@ebitsistemas.com.br>
2024-12-06 16:39:44 -0300

    Commit Initial

--- app/Http/Controllers/MeuPerfilController.php
+++ app/Http/Controllers/MeuPerfilController.php
@@ -10,32 +10,34 @@ use App\Models\GestaoGestor;
 
 class MeuPerfilController extends Controller
 {
-    
+
     public function index()
     {
         $dados["usuario"] = auth()->user();
         return view("MeuPerfil.Index", $dados);
     }
-       
-    public function update(Request $request, $id){        
-        if($request->password){
-            $req["password"] = Hash::make($request->password);
-            $req             = $request->except(["_token","_method","file"]);
-        }else{
-            $req             = $request->except(["_token","_method","file","password"]);
-        }        
+
+    public function update(Request $request, $id)
+    {
+        if ($request->password) {
+            $req["password"] = $request->password;
+            $req = $request->except(["_token", "_method", "file"]);
+        } else {
+            $req = $request->except(["_token", "_method", "file", "password"]);
+        }
 
         if ($request->hasFile('file') && $request->file->isValid()) {
-            $file               = $request->file('file');
-            $extensao           = $file->getClientOriginalExtension();
-            $nomeImagem         = Str::random(25) . ".".$extensao;
-            $pasta              = "storage/upload/foto/";
+            $file = $request->file('file');
+            $extensao = $file->getClientOriginalExtension();
+            $nomeImagem = Str::random(25) . "." . $extensao;
+            $pasta = "storage/upload/foto/";
             $file->move(public_path($pasta), $nomeImagem);
-            $req["foto"] = $pasta . $nomeImagem;            
-        }        
+            $req["foto"] = $pasta . $nomeImagem;
+        }
+        $req["password"] = Hash::make($request->password);
         GestaoGestor::where("id", auth()->user()->getAuthIdentifier())->update($req);
-        return redirect()->route("index")->with("msg_sucesso","Registro alterado com sucesso");
+        return redirect()->route("index")->with("msg_sucesso", "Registro alterado com sucesso");
     }
 
-    
+
 }
