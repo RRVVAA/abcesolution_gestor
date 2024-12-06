15e7e936305a0410f4314c0cf476367f8e663045
Jeferson Maciel <ti@ebitsistemas.com.br>
2024-12-06 16:27:11 -0300

    Commit Initial

--- app/Http/Controllers/Acl/LoginController.php
+++ app/Http/Controllers/Acl/LoginController.php
@@ -7,42 +7,47 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\GestaoGestor;
 use App\Models\Usuario;
+
 //use App\Models\Log; //adicionar recurso de log de sistema no futuro
 
 
-class LoginController extends Controller{
-    public function index(){
+class LoginController extends Controller
+{
+    public function index()
+    {
         return view('Login');
     }
-    
-    
-    public function login(Request $req ){       
+
+    public function login(Request $req)
+    {
         $this->validate($req, [
-            'email'=>'required',
-            'password'=>'required'
-        ]);     
-        
-        
-        $data = $req->all();       
-       
-        if ( Auth::attempt( [ 'email' => $data['email'] , 'password' => $data['password' ]] ) ) {
+            'email' => 'required',
+            'password' => 'required'
+        ]);
 
-            $gestor_logado = Auth::user(); 
-            session(['gestor' => $gestor_logado]); 
-            return redirect()->route('index');               
+        $data = $req->all();
 
-         } else {
-                session()->forget('gestor_logado');
-                Auth::logout();
-                return redirect('login')->with('msg_erro', 'Usuário inativo.');
-            }
-            //return redirect()->route('/');
+        print_r(Auth::attempt(['email' => $data['email'], 'password' => $data['password']]));
+        exit;
+
+        if (Auth::attempt(['email' => $data['email'], 'password' => $data['password']])) {
+            $gestor_logado = Auth::user();
+            session(['gestor' => $gestor_logado]);
+            return redirect()->route('index');
+
+        } else {
+            session()->forget('gestor_logado');
+            Auth::logout();
+            return redirect('login')->with('msg_erro', 'Usuário inativo.');
         }
+        //return redirect()->route('/');
+    }
 
-    public function logout () {
+    public function logout()
+    {
         //logger
         session()->forget('gestor');
-       Auth::logout();
+        Auth::logout();
         return redirect()->route('login');
 
     }
